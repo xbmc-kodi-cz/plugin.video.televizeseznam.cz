e3bab83996180ac0b231983a517978a7db99eaf6
Gabriel <gmendrala@gmail.com>
2019-11-26 20:28:47 +0100

    [plugin.video.televizeseznam.cz] 1.0.15

--- addon.py
+++ addon.py
@@ -37,12 +37,12 @@ def logErr(msg):
     log(msg,level=xbmc.LOGERROR)
 
 def listContent():
-    addDir(_lang(30003)+" "+"Stream", { "urlid": "VGFnOjI", "url": "stream", "category": "service" }, MODE_LIST_CHANNELS)
     addDir(_lang(30002)+" "+"Stream", { "urlid": "VGFnOjI", "url": "stream", "category": "service" }, MODE_LIST_CHANNEL_EPISODES_LATEST)
-    addDir(_lang(30005), { "urlid": "seznam-zpravy", "url": "seznam-zpravy", "category": "service" }, MODE_LIST_CHANNEL_EPISODES_LATEST)
+    addDir(_lang(30003)+" "+"Stream", { "urlid": "VGFnOjI", "url": "stream", "category": "service" }, MODE_LIST_CHANNELS)
     addDir(_lang(30002), { "urlid": "ListShowLatest", "url": "ListShowLatest", "category": "show" }, MODE_LIST_EPISODES_LATEST)
     addDir(_lang(30003), { "urlid": "ListShowLatest", "url": "ListShowLatest", "category": "show" }, MODE_LIST_SHOWS)
     addDir(_lang(30004), { "urlid": "ListShowLatest", "url": "listCategories", "category": "show" }, MODE_LIST_CATEGORIES)
+    addDir(_lang(30005), { "urlid": "seznam-zpravy", "url": "seznam-zpravy", "category": "service" }, MODE_LIST_CHANNEL_EPISODES_LATEST)
 
 def listCategories():    
     client = GraphQLClient(_apiurl)
@@ -169,7 +169,7 @@ def listChannels(urlid, url, category):
         
 def listPlaylistEpisodes(url):
     client = GraphQLClient(_apiurl)
-    params = { "urlName": url, "episodesConnectionFirst": 25 }
+    params = { "urlName": url, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadTag($urlName : String, $episodesConnectionFirst : Int){ tagData:tag(urlName: $urlName, category: tag){ ...PlaylistDetailFragmentOnTag episodesConnection(first : $episodesConnectionFirst) { ...EpisodeCardsFragmentOnEpisodeItemConnection } } }
 		fragment PlaylistDetailFragmentOnTag on Tag {
@@ -262,7 +262,7 @@ def listPlaylistEpisodes(url):
 
 def listEpisodes(url):
     client = GraphQLClient(_apiurl)
-    params = { "urlName": url, "episodesConnectionFirst": 25 }
+    params = { "urlName": url, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadTag($urlName : String, $episodesConnectionFirst : Int){ tagData:tag(urlName: $urlName, category: show){ ...ShowDetailFragmentOnTag episodesConnection(first : $episodesConnectionFirst) { ...SeasonEpisodeCardsFragmentOnEpisodeItemConnection } } }
 		fragment ShowDetailFragmentOnTag on Tag {
@@ -362,7 +362,7 @@ def listEpisodes(url):
 
 def listChannelEpisodesLatest(url, category):
     client = GraphQLClient(_apiurl)
-    params = {"urlName": url, "childTagsConnectionFirst": 1, "episodesConnectionFirst": 25}
+    params = { "urlName": url, "childTagsConnectionFirst": 1, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadChildTags($urlName : String, $childTagsConnectionFirst : Int, $episodesConnectionFirst : Int){ tag(urlName: $urlName, category: '''+category+'''){ childTagsConnection(first : $childTagsConnectionFirst) { ...TimelineBoxFragmentOnTagConnection  edges { node { episodesConnection(first : $episodesConnectionFirst) { ...EpisodeCardsFragmentOnEpisodeItemConnection } } } } } }
 		fragment TimelineBoxFragmentOnTagConnection on TagConnection {
@@ -454,7 +454,7 @@ def listChannelEpisodesLatest(url, category):
 
 def listEpisodesLatest():
     client = GraphQLClient(_apiurl)
-    params = { "limit": 1, "episodesConnectionFirst": 25 }
+    params = { "limit": 1, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadTags($limit : Int, $episodesConnectionFirst : Int){ tags(listing: homepage, limit: $limit){ ...TimelineBoxFragmentOnTag episodesConnection(first : $episodesConnectionFirst) { ...EpisodeCardsFragmentOnEpisodeItemConnection } } tagsCount(listing: homepage) }
 		fragment TimelineBoxFragmentOnTag on Tag {
@@ -532,7 +532,7 @@ def listEpisodesLatest():
         
 def listEpisodesNext(urlid, url):
     client = GraphQLClient(_apiurl)
-    params = { "id": urlid, "episodesConnectionAfter": url, "episodesConnectionFirst": 25 }
+    params = { "id": urlid, "episodesConnectionAfter": url, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadTag($id : ID, $episodesConnectionAfter : String, $episodesConnectionFirst : Int){ tagData:tag(id: $id){ episodesConnection(after: $episodesConnectionAfter,first : $episodesConnectionFirst) { ...SeasonEpisodeCardsFragmentOnEpisodeItemConnection } } }
 		fragment SeasonEpisodeCardsFragmentOnEpisodeItemConnection on EpisodeItemConnection {
@@ -600,7 +600,7 @@ def listEpisodesNext(urlid, url):
         
 def listChannelEpisodesNext(urlid, url):
     client = GraphQLClient(_apiurl)
-    params = { "id": urlid, "episodesConnectionAfter": url, "episodesConnectionFirst": 25 }
+    params = { "id": urlid, "episodesConnectionAfter": url, "episodesConnectionFirst": 20 }
 
     data = client.execute('''query LoadTag($id : ID, $episodesConnectionAfter : String, $episodesConnectionFirst : Int){ tagData:tag(id: $id){ episodesConnection(after: $episodesConnectionAfter,first : $episodesConnectionFirst) { ...EpisodeCardsFragmentOnEpisodeItemConnection } } }
 		fragment EpisodeCardsFragmentOnEpisodeItemConnection on EpisodeItemConnection {
@@ -756,10 +756,10 @@ def videoLink(url):
         link = url[u'Location'].split('/')
         url = getVideoLink(url[u'Location'])
     
-    for quality in sorted(url[u'data'][u"mp4"], key=lambda kv: kv[1], reverse=True):
-        stream_quality=quality
-    
-    if _addon.getSetting('best_quality') == 'false':
+    if _addon.getSetting('best_quality') == 'true':
+        for quality in sorted(url[u'data'][u"mp4"], key=lambda kv: kv[1], reverse=True):
+            stream_quality=quality
+    else:
         stream_quality = _addon.getSetting('lower_quality')
     
     video_url = url[u'data'][u"mp4"][stream_quality][u"url"][3:]
@@ -791,7 +791,7 @@ def getParams():
 def composePluginUrl(urlid, url, category, mode, name):
     return sys.argv[0]+"?urlid="+urllib.quote_plus(urlid.encode('utf-8'))+"&url="+urllib.quote_plus(url.encode('utf-8'))+"&category="+urllib.quote_plus(category.encode('utf-8'))+"&mode="+str(mode)+"&name="+urllib.quote_plus(name.encode('utf-8'))
 
-def addItem(name, url, mode, iconimage, desc, isfolder, islatest=False, info={}):  
+def addItem(name, url, mode, iconimage, desc, isfolder, islatest=False, info={}):
     u = composePluginUrl(url[u'urlid'], url[u'url'], url[u'category'], mode, name)
     ok=True
     liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
@@ -907,7 +907,7 @@ elif mode == MODE_LIST_CHANNEL_NEXT:
     listChannelEpisodesNext(urlid, url)
 
 elif mode == MODE_VIDEOLINK:
-    logDbg('videoLink() with url ' + str(url))
+    logDbg('videoLink()')
     videoLink(url)
     
 xbmcplugin.endOfDirectory(addonHandle)
